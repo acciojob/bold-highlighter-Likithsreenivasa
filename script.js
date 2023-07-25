2289047be3a45f576a82d727d12838ce66b72414
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-25 07:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 function highlight() {
     //Write your code here
-
-
-}
-
-
-function return_normal() {
+	const elements=document.querySelectorAll('strong');
+	for(const element of elements)
+		{
+			const textWrapper=document.createElement('span');
+			textWrapper.textContent=element.textContent;
+			element.textContent=""
+			textWrapper.style.color='green';
+			element.appendChild(textWrapper);
+			
+		}
+	
     //Write your code here
 
     
+}
+function return_normal() {
+		const blackElements=document.querySelectorAll('strong');
+		for(const element of blackElements)
+			{
+				const textWrapper=element.querySelector('span');
+				if(textWrapper)
+				{
+					element.textContent=textWrapper.textContent;
+				}
+				
+			}
+	
 }
